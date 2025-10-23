0b88ae5c449204ead9ba656dc80373dc501b996c
Ahmed-Zayne El husseini <113213346+Ln0Z@users.noreply.github.com>
2025-10-24 00:36:11 +1100

    Commenced building template for implementation

---
+++ header/cuckoo_hash.hpp
@@ -0,0 +1,28 @@
+#ifndef CUCKOO_HASH
+#define CUCKOO_HASH
+#include <vector>
+
+class CuckooHash{
+
+    private: 
+        std::vector<int> h1;
+        std::vector<int> h2;
+        size_t size_;
+        float max_load;
+
+    public: 
+        CuckooHash(){};
+
+        CuckooHash(std::initializer_list<int> vals){
+            for (int x : vals){
+                insert(x);
+            }
+        }
+
+        void insert(int key);
+
+        bool contains();
+
+};
+
+#endif
