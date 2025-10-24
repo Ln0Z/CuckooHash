054989f610172d7ee673cc47399fbfb80945db75
Ahmed <ahmedzayne1999@gmail.com>
2025-10-24 17:59:00 +1100

    Implemented all methods from header file into main file

---
+++ implementation/cuckoo_hash.cpp
@@ -0,0 +1,41 @@
+#include "../header/cuckoo_hash.hpp"
+
+void CuckooHash::insert(int key){
+
+}
+
+bool CuckooHash::contains(int key){
+
+}
+
+bool CuckooHash::erase(int key){
+
+}
+
+//Helpter methods
+void CuckooHash::rehash(){
+
+}
+
+void CuckooHash::clear(){
+
+}
+
+bool CuckooHash::empty(){
+
+}
+
+size_t CuckooHash::size() const {
+
+}
+
+float CuckooHash::load_factor() const{
+
+}
+
+size_t CuckooHash::hash_1(int key){
+
+}
+size_t CuckooHash::hash_2(int key){
+
+}
