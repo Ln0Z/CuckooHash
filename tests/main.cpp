bbfcf2cfb4f973b0bb521a7aa856840a672a0e7c
Ahmed-Zayne El husseini <113213346+Ln0Z@users.noreply.github.com>
2025-11-07 20:37:03 +1100

    Adding tests for insertion

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <random>
 #include "cuckoo_hash.hpp"
 
 TEST(basic_insert_test, insert_10_elements) {
@@ -15,12 +16,12 @@ TEST(basic_insert_test, insert_10_elements) {
                                              std::nullopt, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1,
                                              std::nullopt, std::nullopt};
 
-  ASSERT_EQ(table.return_h1().size(), 13);
-  ASSERT_EQ(table.return_h2().size(), 13);
+  ASSERT_EQ(table.h1_bucket().size(), 13);
+  ASSERT_EQ(table.h2_bucket().size(), 13);
 
     for (size_t i = 0; i < h1_results.size(); ++i){
-        ASSERT_EQ(table.return_h1()[i], h1_results[i]);
-        ASSERT_EQ(table.return_h2()[i], h2_results[i]);
+        ASSERT_EQ(table.h1_bucket()[i], h1_results[i]);
+        ASSERT_EQ(table.h2_bucket()[i], h2_results[i]);
     }
 }
 
@@ -38,14 +39,14 @@ TEST(basic_rehash_test, insert_cause_rehash) {
                                              std::nullopt, std::nullopt};
 
 
-  ASSERT_EQ(table.return_h1().size(), 13);
-  ASSERT_EQ(table.return_h2().size(), 13);
+  ASSERT_EQ(table.h1_bucket().size(), 13);
+  ASSERT_EQ(table.h2_bucket().size(), 13);
   ASSERT_EQ(table.capacity(), 13);
 
 
   for (size_t i = 0; i < h1_results.size(); ++i) {
-    ASSERT_EQ(table.return_h1()[i], h1_results[i]);
-    ASSERT_EQ(table.return_h2()[i], h2_results[i]);
+    ASSERT_EQ(table.h1_bucket()[i], h1_results[i]);
+    ASSERT_EQ(table.h2_bucket()[i], h2_results[i]);
   }
 }
 
@@ -63,13 +64,13 @@ TEST(basic_remove_test, insert_10_elements) {
                                              std::nullopt, std::nullopt, std::nullopt, std::nullopt, std::nullopt, 1,
                                              std::nullopt, std::nullopt};
 
-  ASSERT_EQ(table.return_h1().size(), 13);
-  ASSERT_EQ(table.return_h2().size(), 13);
+  ASSERT_EQ(table.h1_bucket().size(), 13);
+  ASSERT_EQ(table.h2_bucket().size(), 13);
 
 
   for (size_t i = 0; i < h1_results.size(); ++i) {
-    ASSERT_EQ(table.return_h1()[i], h1_results[i]);
-    ASSERT_EQ(table.return_h2()[i], h2_results[i]);
+    ASSERT_EQ(table.h1_bucket()[i], h1_results[i]);
+    ASSERT_EQ(table.h2_bucket()[i], h2_results[i]);
   }
 
   for (size_t i = 0; i < values.size(); ++i) {
@@ -77,10 +78,30 @@ TEST(basic_remove_test, insert_10_elements) {
   }
 
   for (size_t i = 0; i < h1_results.size(); ++i) {
-    ASSERT_EQ(table.return_h1()[i], std::nullopt);
-    ASSERT_EQ(table.return_h2()[i], std::nullopt);
+    ASSERT_EQ(table.h1_bucket()[i], std::nullopt);
+    ASSERT_EQ(table.h2_bucket()[i], std::nullopt);
   }
 }
+
+// Tests for cuckoo hash table,
+// Insert 1 element
+
+TEST(insert_test, insert_single_element){
+  CuckooHash table;
+  
+  ASSERT_FALSE(table.contains(4));
+
+  table.insert(4);
+  ASSERT_TRUE(table.contains(4));
+  ASSERT_EQ(table.size(), 1);
+  ASSERT_FALSE(table.empty());
+
+  size_t idx1 = table.get_hash_1(4);
+  ASSERT_EQ(table.h1_bucket()[idx1].value(), 4);
+}
+
+
+
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
